735e26585a00626a73f011fb6e9f83b4eb653c37
Dean Zheng <danteackles@163.com>
2023-01-16 13:39:09 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,4 +15,4 @@
 
 rm -rf feeds/packages/lang/golang
 svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
-git clone https://github.com/sbwml/luci-app-alist package/alist
+
